2d830bb36b8760ee95ee6d34755d10825b290346
Cyborg2017 <wangyang0987654@gmail.com>
2023-04-12 20:12:12 +0800

    fs: Import nubia changes form ztemt/NX616J_P_kernel
    Change-Id: Idd1bdeaf54423c638ca785468452e0996162ebbd

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -113,6 +113,15 @@
 
 #define REVERT_CRED(saved_cred)	revert_fsids(saved_cred)
 
+//Nubia FileObserver Begin
+#ifdef ENABLE_FILE_OBSERVER
+struct sdcardfs_file_creator {
+    uid_t uid;
+    pid_t pid;
+};
+#endif
+//Nubia FileObserver End
+
 /* Android 5.0 support */
 
 /* Permission mode for a specific node. Controls how file permissions
@@ -180,6 +189,13 @@ extern int sdcardfs_interpose(struct dentry *dentry, struct super_block *sb,
 struct sdcardfs_file_info {
 	struct file *lower_file;
 	const struct vm_operations_struct *lower_vm_ops;
+
+//Nubia FileObserver Begin
+    #ifdef ENABLE_FILE_OBSERVER
+    struct sdcardfs_file_creator creator;
+    __u32 mask;
+    #endif
+//Nubia FileObserver End
 };
 
 struct sdcardfs_inode_data {
